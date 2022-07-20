ba157e9bfd7a8d3beb090d7544cfb8550a51ba37
Ty Lamontagne <amfobes@gmail.com>
2022-07-20 12:28:33 -0400

    feature: Use the model name as a filename instead of BIOS

---
+++ modelname.h
@@ -0,0 +1,10 @@
+/*
+ Modelname Reader, modified by Fobes.
+ Originally licensed as AFL3 under the PS2IDENT project
+*/
+
+#pragma once
+
+#define MODEL_NAME_MAX_LEN 17
+
+int modelname_read(char *name);
